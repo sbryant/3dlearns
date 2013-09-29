4f0000a5536e423931179ae408c8834498939d6f
Sean Bryant <sbryant@github.com>
2013-09-29 12:14:48 -0700

    Add OpenGL and SDL support.

--- src/mfgd.c
+++ src/mfgd.c
@@ -1,7 +1,10 @@
 #include <stddef.h>
+#include <GL/glew.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <math.h>
+#include <SDL.h>
 
 typedef struct s_vector {
     float x;
@@ -91,12 +94,68 @@ vector *vector_normalize(vector* const v) {
 
 int main(int argc, char** argv) {
 
+    SDL_Init(SDL_INIT_VIDEO);
+
+    /* Setup OpenGL 3.2 Context For OS X */
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+
+    /* Create a fullscreen window */
+    SDL_Window *screen = SDL_CreateWindow("MFGD",
+                                          SDL_WINDOWPOS_UNDEFINED,
+                                          SDL_WINDOWPOS_UNDEFINED,
+                                          0, 0,
+                                          SDL_WINDOW_FULLSCREEN_DESKTOP | SDL_WINDOW_OPENGL);
+
+
+    /* initialize a render context, mark as unusued to surpress warnings */
+    SDL_GLContext __attribute__((unused)) *opengl3_context = SDL_GL_CreateContext(screen);
+
+    GLenum glew_status = glewInit();
+    if (glew_status != GLEW_OK) {
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR,
+                                 "GLEW Error",
+                                 "Couldn't initialize GLEW",
+                                 NULL);
+        return -1;
+    }
+
+
     vector r = { 4.0, 0.0 };
     vector d = { 0.0, -5.0 };
 
     vector *v = vector_add(&d, &r);
 
     printf("vector: (%.2f, %.2f)\n", v->x, v->y);
+
+    while(true) {
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glClearColor(0.0, 0.0, 0.0, 1.0);
+        SDL_GL_SwapWindow(screen);
+
+        SDL_Event event;
+        int quit = 0;
+
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+            case SDL_KEYDOWN:
+                break;
+            case SDL_KEYUP:
+                // if escape is pressed, quit
+                if (event.key.keysym.sym == SDLK_ESCAPE)
+                    quit = 1;
+                break;
+            case SDL_QUIT:
+                quit = 1;
+                break;
+            }
+        }
+
+        if (quit == 1) // if received instruction to quit
+            break;
+    }
 
     free(v);
 
