d2472392e8b49fe6a5592b7ca5dc100a7105a619
Sean Bryant <sbryant@github.com>
2013-10-09 19:42:04 -0700

    Three Dee-ish.
    I've sort of tried to implement the 3d cube from open.gl reference code
    but I'm having a bit of trouble.
    
    Placing the camera is a bit wonky, my coordinates don't really match the
    reference coordinates. I pulled back the camera (+8.0 in z) to observe t
    he rotation applied to the cube around the z-axis.
    
    Setting camera y position component to 0, doing so will result in no cube
    being rendered to the window.
    
    I can't seem to actually rotate the camera to a different position with
    camera position vector (eyes). Moving the center can add a limited sort
    of rotation.

--- src/mfgd.c
+++ src/mfgd.c
@@ -19,6 +19,63 @@ float approach(float goal, float current, float dt) {
     return goal;
 }
 
+mat4x4* look_at(vec3 *pos, vec3 *center, vec3 *up) {
+    mat4x4* p = mat4x4_make_ident(NULL);
+
+    vec3* front = vec3_sub(center, pos);
+
+    vec3* f = vec3_normalize(front);
+    vec3* side = vec3_cross(f, up);
+
+    vec3* s = vec3_normalize(side);
+    vec3* u = vec3_cross(s, f);
+
+    p->x[0] = s->x;
+    p->y[0] = s->y;
+    p->z[0] = s->z;
+
+    p->x[1] = u->x;
+    p->y[1] = u->y;
+    p->z[1] = u->z;
+
+    p->x[2] = -f->x;
+    p->y[2] = -f->y;
+    p->z[2] = -f->z;
+
+    printf("Look At P:\n");
+    mat4x4_print(p);
+    printf("\n");
+
+    mat4x4* res = mat4x4_translate(p, -pos->x, -pos->y, -pos->z);
+
+    mat4x4_cleanup(p);
+    free(front);
+    free(side);
+    free(s);
+    free(u);
+    free(f);
+
+    return res;
+}
+
+
+mat4x4* perspective(const float fovy, const float aspect, const float znear, const float zfar) {
+    assert(aspect != 0.0);
+    assert(znear != zfar);
+
+    float f = 1.0 / tan(fovy / 2.0);
+
+    mat4x4 *res = mat4x4_make_ident(NULL);
+    res->x[0] = f / aspect;
+    res->y[1] = f;
+    res->z[2] = (zfar+znear) / (znear-zfar);
+    res->z[3] = -1.0;
+    res->w[2] = (2 * zfar * znear) / (znear-zfar);
+    res->w[3] = 0.0;
+
+    return res;
+}
+
 int main(int argc, char** argv) {
     SDL_Init(SDL_INIT_VIDEO);
 
@@ -27,16 +84,19 @@ int main(int argc, char** argv) {
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
+    SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 4);
 
     /* Create a fullscreen window */
     SDL_Window *screen = SDL_CreateWindow("MFGD",
                                           SDL_WINDOWPOS_UNDEFINED,
                                           SDL_WINDOWPOS_UNDEFINED,
-                                          0, 0,
-                                          SDL_WINDOW_FULLSCREEN_DESKTOP | SDL_WINDOW_OPENGL);
+                                          800, 600,
+                                          SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
 
     /* initialize a render context, managed by SDL */
     SDL_GLContext *opengl3_context = SDL_GL_CreateContext(screen);
+    SDL_GL_SetSwapInterval(1);
 
     /* Use GLEW to setup gl Functions */
     glewExperimental = GL_TRUE;
@@ -69,22 +129,48 @@ int main(int argc, char** argv) {
     glBindVertexArray( vao );
 
     float verts[] = {
-        -0.5f,  0.5f, 1.0, 0.0, 0.0,
-         0.5f,  0.5f, 0.0, 1.0, 0.0,
-         0.5f, -0.5f, 0.0, 0.0, 1.0,
-        -0.5f, -0.5f, 1.0, 1.0, 0.0
+        -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
+         0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
+         0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
+         0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
+
+        -0.5f, -0.5f,  0.5f, 1.0f, 0.0f, 0.0f,
+         0.5f, -0.5f,  0.5f, 1.0f, 0.0f, 0.0f,
+         0.5f,  0.5f,  0.5f, 1.0f, 0.0f, 0.0f,
+         0.5f,  0.5f,  0.5f, 1.0f, 0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f, 1.0f, 0.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f, 1.0f, 0.0f, 0.0f,
+
+        -0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f,
+
+         0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f,
+         0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
+         0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
+         0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
+         0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f,
+
+        -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
+         0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
+         0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f,
+         0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
+
+        -0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
+         0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f
     };
-
-    GLuint elements[] = {
-        0, 1, 2,
-        2, 3, 0
-    };
-
-    GLuint ebo;
-    glGenBuffers(1, &ebo);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elements), elements, GL_STATIC_DRAW);
 
     GLuint vbo;
     glGenBuffers(1, &vbo);
@@ -119,46 +205,90 @@ int main(int argc, char** argv) {
 
     GLint pos_attr = glGetAttribLocation( shader_prog, "position" );
     glEnableVertexAttribArray(pos_attr);
-    glVertexAttribPointer( pos_attr, 2, GL_FLOAT, GL_FALSE, 5*sizeof(float), 0 );
+    glVertexAttribPointer( pos_attr, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), 0 );
 
     GLint color_attr = glGetAttribLocation( shader_prog, "color" );
     glEnableVertexAttribArray(color_attr);
-    glVertexAttribPointer( color_attr, 3, GL_FLOAT, GL_FALSE, 5*sizeof(float), (void*)(2 * sizeof(float)));
+    glVertexAttribPointer( color_attr, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)(3 * sizeof(float)));
 
-    GLuint transform_uni = glGetUniformLocation(shader_prog, "trans");
+    GLuint model_uni = glGetUniformLocation(shader_prog, "model");
+    GLuint view_uni = glGetUniformLocation(shader_prog, "view");
+    GLuint proj_uni = glGetUniformLocation(shader_prog, "proj");
+
+    vec3 camera = { 0.000001, 0.00001, 8.0 };
+    vec3 center = { 0.0, 0.0, 0.0 };
+    vec3 up = { 0.0, 0.0, 1.0 };
+    vec3 axis = { 0.0, 0.0, 1.0 };
+
+    mat4x4* view = look_at(&camera, &center, &up);
+    printf("View Matrix:\n");
+    mat4x4_print(view);
+    printf("\n");
+    float* view_data = mat4x4_make_array(view);
+    mat4x4_cleanup(view);
+
     mat4x4 *ident = mat4x4_make_ident(NULL);
-    vec4 up = { 0.0, 0.0, 1.0 };
+    float *ident_data = mat4x4_make_array(ident);
 
-    unsigned long tick = SDL_GetPerformanceFrequency();
+    float fovy_rad = 45.0 * PI / 180.0;
+    mat4x4 *proj =  perspective(fovy_rad, 800.0 / 600.0 , 1.0, 10.0f);
+    printf("PerspectiveProj Matrix:\n");
+    mat4x4_print(proj);
+    printf("\n");
+    float* proj_data = mat4x4_make_array(proj);
+    mat4x4_cleanup(proj);
+
+    uint64_t old = SDL_GetPerformanceCounter();
+     unsigned long tick = SDL_GetPerformanceFrequency();
+    int pause = 0;
+
+    uint64_t now = SDL_GetPerformanceCounter();
+    float factor = (float)now / tick;
+    float a =  20.0 * (PI/180.0);
+
+    glEnable(GL_DEPTH_TEST);
+
     /* get handle to hold verts we upload */
     while(true) {
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glClearColor(0.0, 0.0, 0.0, 1.0);
 
         glUseProgram( shader_prog );
+        //  float dt = (float)(now - old) / tick;
 
-        unsigned long  t = SDL_GetPerformanceCounter();
-        float rot = (float)t / tick;
-        float a =  180 * (PI/180.0);
-        mat4x4 *trans = mat4x4_rotate(ident, rot * a, &up);
+        if (!pause)
+            now = SDL_GetPerformanceCounter();
+        factor = (float)now / tick;
+        old = now;
+
+
+        mat4x4 *trans = mat4x4_rotate(ident, factor * a, &axis);
         float *data =  mat4x4_make_array(trans);
 
-        glUniformMatrix4fv(transform_uni, 1, GL_FALSE, data);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        //view_data = ident_data;
+        //proj_data = ident_data;
+        glUniformMatrix4fv(view_uni, 1, GL_FALSE, view_data);
+        glUniformMatrix4fv(proj_uni, 1, GL_FALSE, proj_data);
+        glUniformMatrix4fv(model_uni, 1, GL_FALSE, data);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
 
         SDL_GL_SwapWindow(screen);
 
         SDL_Event event;
         int quit = 0;
-        free(trans);
+        mat4x4_cleanup(trans);
         free(data);
 
         while (SDL_PollEvent(&event)) {
             switch (event.type) {
             case SDL_KEYDOWN:
+                if (event.key.keysym.sym == SDLK_SPACE)
+                    pause = 1;
                 break;
             case SDL_KEYUP:
                 // if escape is pressed, quit
+                if (event.key.keysym.sym == SDLK_SPACE)
+                    pause = 0;
                 if (event.key.keysym.sym == SDLK_ESCAPE)
                     quit = 1;
                 break;
@@ -172,7 +302,8 @@ int main(int argc, char** argv) {
             break;
     }
 
-    free(ident);
+    mat4x4_cleanup(ident);
+    free(ident_data);
 
     glDeleteProgram(shader_prog);
     glDeleteShader(frag_shader);
