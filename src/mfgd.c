062efcbd58092222deee73392da5b7e0504ce648
Sean Bryant <sbryant@github.com>
2013-10-10 00:31:14 -0700

    Camera Y can't be 0 for some reason..

--- src/mfgd.c
+++ src/mfgd.c
@@ -211,8 +211,8 @@ int main(int argc, char** argv) {
     GLuint view_uni = glGetUniformLocation(shader_prog, "view");
     GLuint proj_uni = glGetUniformLocation(shader_prog, "proj");
 
-    vec3 camera = { 0.000001, 0.00001, 8.0 };
-    vec3 center = { 0.0, 0.0, 0.0 };
+    vec3 camera = { 0.0, 0.0, 8.0 };
+    vec3 center = { 0.0, 0.0000001, 0.0};
     vec3 up = { 0.0, 0.0, 1.0 };
     vec3 axis = { 0.0, 0.0, 1.0 };
 
