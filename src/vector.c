1c382beeee242fc4064abed38f3f1c400d7feff5
Sean Bryant <sbryant@github.com>
2013-10-09 19:00:36 -0700

    vec3 operations.

--- src/vector.c
+++ src/vector.c
@@ -11,6 +11,12 @@ float vec4_length(vec4* const v) {
                  (v->w * v->w));
 }
 
+float vec3_length(vec3* const v) {
+    return sqrtf((v->x * v->x) +
+                 (v->y * v->y) +
+                 (v->z * v->z));
+}
+
 float vec4_length_square(vec4* const v) {
     return (v->x * v->x) +
         (v->y * v->y) +
@@ -40,6 +46,16 @@ vec4* vec4_sub(vec4* const v1, vec4* const v2) {
     return vr;
 }
 
+vec3* vec3_sub(vec3* const v1, vec3* const v2) {
+    vec3 *vr = vec3_make();
+
+    vr->x = v1->x - v2->x;
+    vr->y = v1->y - v2->y;
+    vr->z = v1->z - v2->z;
+
+    return vr;
+}
+
 /* Vec4 scaling functions */
 
 vec4 *vec4_mul_scalar(vec4* const v, float const s) {
@@ -64,9 +80,13 @@ vec4 *vec4_div_scalar(vec4* const v, float const s) {
     return v2;
 }
 
+vec3* vec3_make(void) {
+    return (vec3 *)calloc(1, sizeof(vec3));
+}
+
 
 vec4 *vec4_normalize(vec4* const v) {
-    vec4 *v2  = (vec4 *)calloc(1, sizeof(vec4));
+    vec4 *v2  = vec4_make();
     float v_len = vec4_length(v);
 
     v2->x = v->x / v_len;
@@ -75,6 +95,30 @@ vec4 *vec4_normalize(vec4* const v) {
     v2->w = v->w / v_len;
 
     return v2;
+}
+
+vec3 *vec3_normalize(vec3* const v) {
+    vec3 *v2  = vec3_make();
+    float v_len = vec3_length(v);
+
+    v2->x = v->x / v_len;
+    v2->y = v->y / v_len;
+    v2->z = v->z / v_len;
+
+    return v2;
+}
+
+float vec4_dot(vec4* const a, vec4* const b) {
+    return (a->x * b->x) + (a->y * b->y) + (a->z * b->z) + (a->w * b->w);
+}
+
+vec3* vec3_cross(vec3* const a, vec3* const b) {
+    vec3 *v  = vec3_make();
+    v->x = (a->y * b->z) - (b->y * a->z);
+    v->y = (a->z * b->x) - (b->z * a->x);
+    v->z = (a->x * b->y) - (b->x * a->y);
+
+    return v;
 }
 
 void vec4_print(vec4* const v) {
--- src/vector.h
+++ src/vector.h
@@ -6,15 +6,27 @@ typedef struct s_vec4 {
     float w;
 } vec4;
 
+typedef struct s_vec3 {
+    float x;
+    float y;
+    float z;
+} vec3;
+
 float vec4_length(vec4* const v);
 float vec4_length_square(vec4* const v);
 
 vec4* vec4_add(vec4* const v1, vec4* const v2);
 vec4* vec4_sub(vec4* const v1, vec4* const v2);
+vec3* vec3_sub(vec3* const v1, vec3* const v2);
 
 vec4 *vec4_mul_scalar(vec4* const v, float const s);
 vec4 *vec4_div_scalar(vec4* const v, float const s);
 
 vec4 *vec4_normalize(vec4* const v);
+vec3 *vec3_normalize(vec3* const v);
+
+vec3* vec3_cross(vec3* const a, vec3* const b);
+
+vec3* vec3_make(void);
 
 void vec4_print(vec4* const v);
