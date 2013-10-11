504352de73d63b7fdd029211dd203ebfd722b537
Sean Bryant <sbryant@github.com>
2013-10-11 00:42:10 -0700

    Move look_at and perspective helpers out to utils.

--- src/utils.c
+++ src/utils.c
@@ -3,8 +3,11 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <math.h>
+#include <assert.h>
 
 #include "utils.h"
+
 
 char *read_shader(const char* path) {
 	int fd = open(path, O_RDONLY);
@@ -29,3 +32,56 @@ char *read_shader(const char* path) {
 
 	return result;
 };
+
+mat4x4* look_at(vec3 *pos, vec3 *center, vec3 *up) {
+    mat4x4* p = mat4x4_make_ident(NULL);
+
+    vec3* front = vec3_sub(center, pos);
+
+    vec3* f = vec3_normalize(front);
+    vec3* side = vec3_cross(f, up);
+
+    vec3* s = vec3_normalize(side);
+    vec3* u = vec3_cross(s, f);
+
+    p->x[0] = s->x;
+    p->y[0] = s->y;
+    p->z[0] = s->z;
+
+    p->x[1] = u->x;
+    p->y[1] = u->y;
+    p->z[1] = u->z;
+
+    p->x[2] = -f->x;
+    p->y[2] = -f->y;
+    p->z[2] = -f->z;
+
+    mat4x4* res = mat4x4_translate(p, -pos->x, -pos->y, -pos->z);
+
+    mat4x4_cleanup(p);
+    free(front);
+    free(side);
+    free(s);
+    free(u);
+    free(f);
+
+    return res;
+}
+
+
+mat4x4* perspective(const float fovy, const float aspect, const float znear, const float zfar) {
+    assert(aspect != 0.0);
+    assert(znear != zfar);
+
+    float f = 1.0 / tan(fovy / 2.0);
+
+    mat4x4 *res = mat4x4_make_ident(NULL);
+    res->x[0] = f / aspect;
+    res->y[1] = f;
+    res->z[2] = (zfar+znear) / (znear-zfar);
+    res->z[3] = -1.0;
+    res->w[2] = (2 * zfar * znear) / (znear-zfar);
+    res->w[3] = 0.0;
+
+    return res;
+}
--- src/utils.h
+++ src/utils.h
@@ -1,4 +1,10 @@
 #pragma once
+#include "matrix.h"
+#include "vector.h"
+
 #define GLPG_SHADER_READ_SIZE 1024
 
 char *read_shader(const char* path);
+
+mat4x4* look_at(vec3 *pos, vec3 *center, vec3 *up);
+mat4x4* perspective(const float fovy, const float aspect, const float znear, const float zfar);
