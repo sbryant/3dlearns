f0b25e171af3b206bd9db7fdf1b3ea5e73657e97
Sean Bryant <sbryant@github.com>
2013-10-20 18:09:10 -0700

    Render cube, floor and reflection.

--- src/mfgd.c
+++ src/mfgd.c
@@ -72,136 +72,124 @@ int main(int argc, char** argv) {
     glGenVertexArrays( 1, &vao );
     glBindVertexArray( vao );
 
-    float verts[] = {
-        -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
-         0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
-         0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
-         0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
-        -0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
+    GLfloat vertices[] = {
+    -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
+     0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
+     0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
+     0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
+    -0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
 
-        -0.5f, -0.5f,  0.5f, 1.0f, 0.0f, 0.0f,
-         0.5f, -0.5f,  0.5f, 1.0f, 0.0f, 0.0f,
-         0.5f,  0.5f,  0.5f, 1.0f, 0.0f, 0.0f,
-         0.5f,  0.5f,  0.5f, 1.0f, 0.0f, 0.0f,
-        -0.5f,  0.5f,  0.5f, 1.0f, 0.0f, 0.0f,
-        -0.5f, -0.5f,  0.5f, 1.0f, 0.0f, 0.0f,
+    -0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f,
+     0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f,
+    -0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f,
 
-        -0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f,
-        -0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
-        -0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f,
-        -0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f,
+    -0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f,
+    -0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f,
+    -0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f,
 
-         0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f,
-         0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
-         0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
-         0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
-         0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f,
-         0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f,
+     0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
+     0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
+     0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
+     0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f,
 
-        -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
-         0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
-         0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f,
-         0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f,
-        -0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
+     0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
+     0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f,
+     0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
 
-        -0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
-         0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
-         0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f,
-         0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f,
-        -0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f,
-        -0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f
+    -0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
+     0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f,
+    -0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f,
+    -0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
+
+    -1.0f, -1.0f, -0.5f, 0.0f, 0.0f, 0.0f,
+     1.0f, -1.0f, -0.5f, 0.0f, 0.0f, 0.0f,
+     1.0f,  1.0f, -0.5f, 0.0f, 0.0f, 0.0f,
+     1.0f,  1.0f, -0.5f, 0.0f, 0.0f, 0.0f,
+    -1.0f,  1.0f, -0.5f, 0.0f, 0.0f, 0.0f,
+    -1.0f, -1.0f, -0.5f, 0.0f, 0.0f, 0.0f
     };
-
-    GLuint vbo;
-    glGenBuffers(1, &vbo);
-
-    glBindBuffer(GL_ARRAY_BUFFER, vbo );
-    glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
 
     shader *s = make_shader("model", "shaders/simple_vert.glsl", "shaders/simple_frag.glsl");
     shader_compile(s);
 
     glUseProgram(s->program);
 
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
     glEnableVertexAttribArray(s->pos_attr);
-    glVertexAttribPointer( s->pos_attr, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), 0 );
+    glVertexAttribPointer(s->pos_attr, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, 0);
 
     glEnableVertexAttribArray(s->color_attr);
-    glVertexAttribPointer( s->color_attr, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)(3 * sizeof(float)));
+    glVertexAttribPointer(s->color_attr, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, (void*)(sizeof(float) * 3));
 
+    memset(&box, 0, sizeof(character));
+    box.gravity.y = -4.0;
 
-    GLuint model_uni = glGetUniformLocation(s->program, "model");
-    GLuint view_uni = glGetUniformLocation(s->program, "view");
-    GLuint proj_uni = glGetUniformLocation(s->program, "proj");
+    int w, h;
+    get_screen_size(&w, &h);
+    struct s_renderer *r = make_renderer(w, h);
+    renderer_initialize(r);
 
-    vec3 camera = { 0.0, 0.0, 8.0 };
-    vec3 center = { 0.0, 0.0, 0.0};
-    vec3 up = { 0.0, 1.0, 0.0 };
-    vec3 axis = { 0.0, 1.0, 0.0 };
+    /* hook renderer up with our shader prog */
+    r->shader = s;
 
-    mat4x4* view = look_at(&camera, &center, &up);
+    vec3 cam = { 2.5f, 2.5f, 2.0f };
+    vec3 c_up = { 0.0f, 0.0f, 1.0f };
+    vec3 c_at = { 0.0f, 0.0f, 0.0f };
+
+    mat4x4 *view = look_at(&cam, &c_at, &c_up);
+
     printf("View Matrix:\n");
     mat4x4_print(view);
     printf("\n");
+    fflush(stdout);
 
-    float fovy_rad = 45.0 * PI / 180.0;
-    mat4x4 *proj =  perspective(fovy_rad, 16.0 / 9.0 , 0.1, 1000.0f);
-    printf("PerspectiveProj Matrix:\n");
+    mat4x4 *proj = perspective(45.0f, 800.0f / 600.0f, 1.0, 10.0f);
+    printf("Porj Matrix:\n");
     mat4x4_print(proj);
     printf("\n");
+    fflush(stdout);
 
-    uint64_t old = SDL_GetPerformanceCounter();
-    unsigned long tick = SDL_GetPerformanceFrequency();
-    int pause = 0;
+    GLint uniView = glGetUniformLocation(s->program, "view");
 
-    uint64_t now = SDL_GetPerformanceCounter();
-    float factor = 0.0;
-    float a =  180.0 * (PI/180.0);
-    mat4x4 *ident = mat4x4_make_ident(NULL);
 
-    /* get handle to hold verts we upload */
+    GLint uniProj = glGetUniformLocation(s->program, "proj");
+
+    GLint uniModel = glGetUniformLocation(s->program, "model");
+
+    GLint uniColor = glGetUniformLocation(s->program, "overrideColor");
+
+    int tick = 1000;
+
+    glEnable(GL_DEPTH_TEST);
+
     while(true) {
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glEnable(GL_DEPTH_TEST);
-        glEnable(GL_MULTISAMPLE);
-        glClearColor(0.0, 0.0, 0.0, 1.0);
-
-        glUseProgram( s->program );
-        //  float dt = (float)(now - old) / tick;
-
-        if (!pause) {
-            old = now;
-            now = SDL_GetPerformanceCounter();
-        }
-
-        factor = now / (float)tick;
-
-        mat4x4 *trans = mat4x4_rotate(ident, factor * a, &axis);
-        glUniformMatrix4fv(view_uni, 1, GL_FALSE, view->m);
-        glUniformMatrix4fv(proj_uni, 1, GL_FALSE, proj->m);
-        glUniformMatrix4fv(model_uni, 1, GL_FALSE, trans->m);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
-
-        SDL_GL_SwapWindow(screen);
-
         SDL_Event event;
         int quit = 0;
-        mat4x4_cleanup(trans);
 
         while (SDL_PollEvent(&event)) {
             switch (event.type) {
             case SDL_KEYDOWN:
-                if (event.key.keysym.sym == SDLK_SPACE)
-                    pause = 1;
                 break;
             case SDL_KEYUP:
-                // if escape is pressed, quit
-                if (event.key.keysym.sym == SDLK_SPACE)
-                    pause = 0;
                 if (event.key.keysym.sym == SDLK_ESCAPE)
                     quit = 1;
                 break;
@@ -213,15 +201,52 @@ int main(int argc, char** argv) {
 
         if (quit == 1) // if received instruction to quit
             break;
+
+        uint32_t now = SDL_GetTicks();
+        glClearColor(1.0f, 0.0f, 1.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        mat4x4 *ident = mat4x4_make_ident(NULL);
+        vec3 up = { 0.0f, 0.0f, 1.0f };
+
+        glUniformMatrix4fv(uniView, 1, GL_FALSE, view->m);
+        glUniformMatrix4fv(uniProj, 1, GL_FALSE, proj->m);
+
+        mat4x4 *model = mat4x4_rotate(ident, now / (float)tick * 180.0f, &up);
+        glUniformMatrix4fv(uniModel, 1, GL_FALSE, model->m);
+
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        glEnable(GL_STENCIL_TEST);
+        glStencilFunc(GL_ALWAYS, 1, 0xFF);
+        glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
+        glStencilMask(0xFF);
+        glDepthMask(GL_FALSE);
+        glClear(GL_STENCIL_BUFFER_BIT);
+
+        glDrawArrays(GL_TRIANGLES, 36, 6);
+
+        glStencilFunc(GL_EQUAL, 1, 0xFF);
+        glStencilMask(0x00);
+        glDepthMask(GL_TRUE);
+
+        mat4x4 *trans = mat4x4_translate(model, 0.0, 0.0, -1.0);
+        mat4x4_scale(trans, 1.0, 1.0, -1.0);
+
+        glUniformMatrix4fv(uniModel, 1, GL_FALSE, trans->m);
+
+        glUniform3f(uniColor, 0.3f, 0.3f, 0.3f);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        glUniform3f(uniColor, 1.0f, 1.0f, 1.0f);
+        glDisable(GL_STENCIL_TEST);
+        SDL_GL_SwapWindow(screen);
+
+        mat4x4_cleanup(trans);
+        mat4x4_cleanup(ident);
+        mat4x4_cleanup(model);
     }
 
-    mat4x4_cleanup(view);
-    mat4x4_cleanup(proj);
-    mat4x4_cleanup(ident);
-
     shader_cleanup(s);
-
-    glDeleteBuffers(1, &vbo);
     glDeleteVertexArrays(1, &vao);
 
     free(r);
