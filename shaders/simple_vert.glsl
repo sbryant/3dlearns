7c3840862b9b59a913bb4bc84fbbd73ab121cf21
Sean Bryant <sbryant@github.com>
2013-10-20 17:42:17 -0700

    Update frag shader for temporary expierments.

--- shaders/simple_vert.glsl
+++ shaders/simple_vert.glsl
@@ -5,11 +5,12 @@ in vec3 color;
 
 out vec4 Color;
 
-uniform mat4 model;
-uniform mat4 view;
 uniform mat4 proj;
+uniform mat4 view;
+uniform mat4 model;
+uniform vec3 overrideColor;
 
 void main() {
-    Color = vec4(color, 1.0);
+    Color = vec4(overrideColor * color, 1.0);
     gl_Position = proj * view * model * vec4(position, 1.0);
 }
