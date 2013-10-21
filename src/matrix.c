fb0a28f6c158d7143228c09bcc0a8f367bd06dca
Sean Bryant <sbryant@github.com>
2013-10-21 00:21:39 -0700

    Move look_at and perspective to matrix file.
    Also add type_ prefix.

--- src/matrix.c
+++ src/matrix.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <assert.h>
 #include "matrix.h"
 
 #define mp_at(m,v,i) m->v[i]
@@ -158,4 +159,55 @@ void mat4x4_cleanup(mat4x4* m) {
 
 void mat4x4_set_mat4x4(mat4x4 *m1, const mat4x4 *m2) {
     memcpy(&(m1->m), &(m2->m), sizeof(float) * 16);
+}
+
+mat4x4* mat4x4_look_at(vec3 *pos, vec3 *center, vec3 *up) {
+    mat4x4* p = mat4x4_make_ident(NULL);
+
+    vec3 *ce = vec3_sub(center, pos);
+    vec3 *f = vec3_normalize(ce);
+
+    vec3 *nu = vec3_normalize(up);
+    vec3 *fnu = vec3_cross(f, nu);
+    vec3 *s = vec3_normalize(fnu);
+
+    vec3 *u = vec3_cross(s, f);
+
+    p->x[0] = s->x;
+    p->y[0] = s->y;
+    p->z[0] = s->z;
+
+    p->x[1] = u->x;
+    p->y[1] = u->y;
+    p->z[1] = u->z;
+
+    p->x[2] = -f->x;
+    p->y[2] = -f->y;
+    p->z[2] = -f->z;
+
+    p->w[0] = -vec3_dot(s, pos);
+    p->w[1] = -vec3_dot(u, pos);
+    p->w[2] = vec3_dot(f, pos);
+
+    return p;
+}
+
+mat4x4* mat4x4_perspective(const float fovy, const float aspect, const float znear, const float zfar) {
+    assert(aspect != 0.0);
+    assert(znear != zfar);
+
+    const float rad = fovy * PI / 180.0f;
+    float tan_half_fovy = tan(rad / 2.0f);
+
+    mat4x4 *res = mat4x4_make_ident(NULL);
+    res->x[0] = 1.0f / (aspect * tan_half_fovy);
+    res->y[1] = 1.0f / tan_half_fovy;
+
+    res->z[2] = -(zfar + znear) / (zfar - znear);
+    res->z[3] = -1.0;
+
+    res->w[2] = -(2.0 * zfar * znear) / (zfar - znear);
+    res->w[3] = 0.0;
+
+    return res;
 }
