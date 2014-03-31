bf6b1b2740dfb6951ed60d928f53ba47cc51e267
Sean Bryant <sbryant@github.com>
2014-03-31 00:13:28 -0700

    Remove shader functions.

--- src/utils.h
+++ src/utils.h
@@ -1,11 +1,8 @@
 #pragma once
+#include <unistd.h>
 #include <SDL.h>
 #include "matrix.h"
 #include "vector.h"
 
-
-#define GLPG_SHADER_READ_SIZE 1024
-
-char *read_shader(const char* path);
 void get_screen_size(int *w, int *h);
 float approach(float goal, float current, float dt);
