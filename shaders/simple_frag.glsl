16941e0157be7d78e9a733cc0fc072cb0db379b3
Sean Bryant <sbryant@github.com>
2016-09-03 13:53:24 -0700

    Draw a triangle

--- shaders/simple_frag.glsl
+++ shaders/simple_frag.glsl
@@ -1,8 +1,7 @@
 #version 450
 
+layout(location = 0) in vec4 color;
 layout(location = 0) out vec4 outColor;
-
-in vec4 color;
 
 void main() {
     outColor = color;
--- shaders/simple_vert.glsl
+++ shaders/simple_vert.glsl
@@ -1,15 +1,28 @@
 #version 450
 
+layout(location = 0) out vec4 color;
 
-layout(location = 0) in vec3 in_position;
-layout(location = 1) in vec3 in_color;
-out vec4 color;
+out gl_PerVertex {
+	vec4 gl_Position;
+};
 
-uniform mat4 proj;
-uniform mat4 view;
-uniform mat4 model;
+vec2 positions[3] = vec2[](
+	vec2(0.0, 0.5),
+	vec2(0.5, -0.5),
+	vec2(-0.5, -0.5)
+);
+
+vec3 colors[3] = vec3[](
+	vec3(1.0, 0.0, 0.0),
+	vec3(0.0, 1.0, 0.0),
+	vec3(0.0, 0.0, 1.0)
+);
 
 void main() {
-    color = vec4(in_color, 1.0);
+    /*
+	color = vec4(in_color, 1.0);
     gl_Position = proj * view * model * vec4(in_position, 1.0);
+	*/
+	color = vec4(colors[gl_VertexID], 1.0);
+	gl_Position = vec4(positions[gl_VertexID], 0.0, 1.0);
 }
